f49425690664eb4bdaeab372b6fea11ac11f6a93
Piyush Das <piyushdas98@gmail.com>
2022-05-18 01:37:27 +0530

    feat: Added sinup page: Integrated formik+Yup

--- src/constants/validationMessages.js
+++ src/constants/validationMessages.js
@@ -3,4 +3,6 @@ export const validationMessages = {
     emailFormat: "You made a boo boo entering the email.",
     passwordEmpty: "You seriously think we'll let you in without a password?",
     passwordShort: "Password must have atleast 8 characters",
+    userNameEmpty: "Username cannot be empty like your love life",
+    userNameShort: "Username must be 4 characters atleast"
 }
