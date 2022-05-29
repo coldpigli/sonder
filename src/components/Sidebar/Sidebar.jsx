a28b45aba46ef9d36ec25098f2172ad2583315ca
Piyush Das <piyushdas98@gmail.com>
2022-05-30 01:37:20 +0530

    fix: sidebar orientation and layout

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,8 +6,8 @@ import { NavLink } from "react-router-dom";
 const Sidebar = () => {
   let activeStyle = {
     backgroundColor: "#6C5DD3",
-    borderRadius: '1rem',
-    color: "#FFFFFF"
+    borderRadius: "1rem",
+    color: "#FFFFFF",
   };
 
   return (
@@ -16,8 +16,8 @@ const Sidebar = () => {
       bg="#242731"
       position="sticky"
       p="1rem"
-      top='0'
-      borderRight='0.5px solid rgba(128, 129, 145, 0.25)'
+      top="0"
+      borderRight="0.5px solid rgba(128, 129, 145, 0.25)"
     >
       <VStack h="100%" justify="space-between">
         <Box color="white">
@@ -29,10 +29,17 @@ const Sidebar = () => {
               <NavLink
                 to={itemInfo.path}
                 key={itemInfo.id}
-                style={({ isActive }) => (isActive ? activeStyle : {color: "#808191"})}
+                style={({ isActive }) =>
+                  isActive ? activeStyle : { color: "#808191" }
+                }
               >
                 <Flex p="0.5rem" justifyContent="center">
-                  <Icon as={itemInfo.icon} w={6} h={6} _hover={{color: "white"}}/>
+                  <Icon
+                    as={itemInfo.icon}
+                    w={6}
+                    h={6}
+                    _hover={{ color: "white" }}
+                  />
                 </Flex>
               </NavLink>
             );
