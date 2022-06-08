16416f526f240b37fa23c9d2fd7307f4c349b47b
Piyush Das <piyushdas98@gmail.com>
2022-06-09 00:37:47 +0530

    feat: feed filters

--- src/routes/Feed/Feed.jsx
+++ src/routes/Feed/Feed.jsx
@@ -1,8 +1,30 @@
-import { Box, Heading, HStack, Stack, Text, useMediaQuery, VStack } from "@chakra-ui/react";
-import { AdditionalInfo, CreatePost, GreetingHeader, PostItem, Sidebar, TopBar} from "components";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  Text,
+  useMediaQuery,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  AdditionalInfo,
+  CreatePost,
+  GreetingHeader,
+  PostItem,
+  Sidebar,
+  TopBar,
+} from "components";
 import BottomNav from "components/BottomNav/BottomNav";
+import { BsFilterSquare } from "react-icons/bs";
 import { fallbackData, urls } from "constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts, getAllUsers } from "services";
 
@@ -10,22 +32,32 @@ const Feed = () => {
   const dispatch = useDispatch();
   const [smallerDevice] = useMediaQuery("(max-width: 900px)");
   const { greetingSubHeaderFeed, greetingDescriptionFeed } = fallbackData;
-  const {postList} = useSelector((state)=>state.posts);
+  const { postList } = useSelector((state) => state.posts);
+  const [olderFirst, setOlderFirst] = useState(false);
 
   useEffect(() => {
     dispatch(getAllUsers());
-    if(postList.length===0)
-    {
-      dispatch(getAllPosts())
+    if (postList.length === 0) {
+      dispatch(getAllPosts());
     }
   }, []);
 
   return (
     <Box display="flex" gap="12" color="white">
       {smallerDevice ? <BottomNav /> : <Sidebar />}
-      <VStack minH="100vh" flex="2" gap="4" align="stretch" p={`${smallerDevice ? "1rem 1rem 6rem 1rem" : "1rem 0"}`}>
+      <VStack
+        minH="100vh"
+        flex="2"
+        gap="4"
+        align="stretch"
+        p={`${smallerDevice ? "1rem 1rem 6rem 1rem" : "1rem 0"}`}
+      >
         <TopBar />
-        <GreetingHeader greetingSubHeader={greetingSubHeaderFeed} greetingDescription={greetingDescriptionFeed} greetingImg={urls.helloBoy}/>
+        <GreetingHeader
+          greetingSubHeader={greetingSubHeaderFeed}
+          greetingDescription={greetingDescriptionFeed}
+          greetingImg={urls.helloBoy}
+        />
         <Box>
           <CreatePost />
         </Box>
@@ -33,11 +65,44 @@ const Feed = () => {
           <Box mt="1rem">
             <HStack justify="space-between" mb="1rem">
               <Heading size="md">Scroll</Heading>
-              <Text fontSize="sm">Showing Recent</Text>
+              <HStack>
+                <Text>{olderFirst ? "Older first" : "Showing Recent"}</Text>
+                <Menu>
+                  <MenuButton
+                    as={IconButton}
+                    icon={<BsFilterSquare />}
+                    bg="lightBlue"
+                    _focus={{ boxShadow: "none" }}
+                    _hover={{ bg: "none" }}
+                    _active={{ bg: "none" }}
+                  ></MenuButton>
+                  <MenuList bg="lightBlue">
+                    <MenuItem
+                      _hover={{ backgroundColor: "#242731", color: "white" }}
+                      _focus={{ backgroundColor: "#242731", color: "white" }}
+                      onClick={() => setOlderFirst(false)}
+                    >
+                      Recent
+                    </MenuItem>
+                    <MenuItem
+                      _hover={{ backgroundColor: "#242731", color: "white" }}
+                      _focus={{ backgroundColor: "#242731", color: "white" }}
+                      onClick={() => setOlderFirst(true)}
+                    >
+                      Older First
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </HStack>
             </HStack>
-            <Stack align="stretch" spacing="6" borderRadius="1rem" direction="column-reverse" >
+            <Stack
+              align="stretch"
+              spacing="6"
+              borderRadius="1rem"
+              direction={olderFirst ? "column" : "column-reverse"}
+            >
               {postList?.map((post) => {
-                return <PostItem post={post} key={post._id}/>;
+                return <PostItem post={post} key={post._id} />;
               })}
             </Stack>
           </Box>
